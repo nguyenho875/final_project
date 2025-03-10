44c5fe551564e39b838643b4f6625fcb3719d7ab
DwyerJd <145938286+DwyerJd@users.noreply.github.com>
2025-03-10 17:32:38 -0400

    Update motor.h

--- modules/motor/motor.h
+++ modules/motor/motor.h
@@ -1,20 +1,12 @@
 //=====[#include guards - begin]===============================================
-
 #ifndef _MOTOR_H_
 #define _MOTOR_H_
 
-//=====[Declaration of public defines]=========================================
-
-//=====[Declaration of public data types]======================================
-
 //=====[Declarations (prototypes) of public functions]=========================
-
 void motorControlInit();
-void motorControl(); 
-int floorLevelRead();
-void checkAndBringDown();
 void bringToLevel(int level);
+void returnToFirstFloor();
+int floorLevelRead();
 
 //=====[#include guards - end]=================================================
-
 #endif // _MOTOR_H_
