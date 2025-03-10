d2f354f9e41826bceb290cc4e008b04951998db9
DwyerJd <145938286+DwyerJd@users.noreply.github.com>
2025-03-10 17:30:50 -0400

    Update display.h

--- modules/display/display.h
+++ modules/display/display.h
@@ -1,19 +1,12 @@
-//=====[#include guards - begin]===============================================
-
+//=====[#include guards - begin]=============================================
 #ifndef _DISPLAY_H_
 #define _DISPLAY_H_
 
-//=====[Declaration of public defines]=========================================
-
-//=====[Declaration of public data types]======================================
-
 //=====[Declarations (prototypes) of public functions]=========================
-
-void displayInit( void );
-void displayWelcome();
- 
-
+void displayInit();
+void displayClear();
+void displayStringWrite(const char * str);
+void displayCharPositionWrite(unsigned char charPositionX, unsigned char charPositionY);
 
 //=====[#include guards - end]=================================================
-
 #endif // _DISPLAY_H_
