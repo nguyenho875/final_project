7464e449f529f1c6b8e00f9757836192686f6479
DwyerJd <145938286+DwyerJd@users.noreply.github.com>
2025-03-10 17:31:47 -0400

    Update leds.h

--- modules/leds/leds.h
+++ modules/leds/leds.h
@@ -1,18 +1,11 @@
 //=====[#include guards - begin]===============================================
-
 #ifndef _LEDS_H_
 #define _LEDS_H_
 
-//=====[Declaration of public defines]=========================================
-
-//=====[Declaration of public data types]======================================
-
 //=====[Declarations (prototypes) of public functions]=========================
 
-void ledsInit();    // initialize green and red LEDs to off
-void redLedUpdate( bool state );  // turn on/off red led with parameter
-void greenLedUpdate( bool state ) // turn on/off green led with parameter
- 
-//=====[#include guards - end]=================================================
+void ledsInit();
+void ledsUpdate();
 
+//=====[#include guards - end]=================================================
 #endif // _LEDS_H_
